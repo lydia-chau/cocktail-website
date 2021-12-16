02e6a93b145727d3acb085ab5a89e893e36dd2a8
Lydia Chau <lydia.chau@concordusa.com>
2021-12-16 16:35:51 -0600

    animation working, popup close animation buggy

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { useState, useCallback } from "react";
+import { useState } from "react";
 import CocktailDetails from './CocktailDetails.js'
 import Axios from "axios";
 import cocktailImage from './cocktail-homepage.jpg';
 import './Home.css'
+import { display } from '@mui/system';
 
 export default function Home() {
     const [cocktail, setCocktail] = useState("");
     const [randomHidden,setRandomHidden]=useState(false);
 
-    const getCocktail = useCallback(() => {
+    const getCocktail = () => {
+      console.log(randomHidden);
       setRandomHidden(false);
       Axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php").then(
         (response) => {
@@ -17,11 +19,11 @@ export default function Home() {
           // console.log(cocktail);
         }
       );
-    }, [randomHidden]);
+    };
     return (
       <div className="App">
         <div className="body">
-          <header className="App-header">
+          <header className={randomHidden && cocktail? 'App-header header-slide-in':randomHidden? 'App-header': cocktail ? 'App-header header-slide-out' : 'App-header'}>
           
             <h1 className='what-drink-header'>What drink will you have today?</h1>
             
@@ -37,17 +39,17 @@ export default function Home() {
               Generate Random Cocktail
             </button> */}
 
-          {cocktail && !randomHidden &&
-              <div className='random-home-div'>
+          {/* {cocktail && !randomHidden && */}
+              <div className={cocktail && randomHidden? 'random-home-div slide-out ': cocktail && !randomHidden? 'random-home-div slide-in': 'hidden'}>
 
                 <img alt='cocktail' className ='cocktail-image-home' src={cocktail.strDrinkThumb}></img>
 
                 <div className='random-cocktail-homepage-card'>
-                  <CocktailDetails cocktail={cocktail} homepage={true} setRandomHidden={setRandomHidden}></CocktailDetails>
+                    <CocktailDetails cocktail={cocktail} homepage={true} setRandomHidden={setRandomHidden}></CocktailDetails>
                 </div>
 
               </div>
-            }
+           {/* } */}
           
 
           <div className="alcohols">
